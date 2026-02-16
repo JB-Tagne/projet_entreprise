1ed6a86115671c966a8e1b9bc2f6f4f6c2bf8ea1
John B <johnborel91@gmail.com>
2026-02-16 11:18:17 +0100

    modification du script par john

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 mon code python
 ajout de code par la collègue
+code ajouté par moi en même temps de la collègue
 script rajouté par moi et modifié plus tard car plusieurs erreurs
 du code supplementaire ecrit par la collegue
